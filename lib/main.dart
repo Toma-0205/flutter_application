f303bd0e8f8cfe0e8767d1717de73de0602373bf
Toma Morishita <25winterhorse@gmail.com>
2024-11-13 22:15:05 +0900

    Incrementできなくなったので再作成

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/main_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+ 
+void main() {
+  const String title = 'Flutter Demo';
+  runApp(const ProviderScope(child: MyApp(title: title,)));
+}
+ 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key,required this.title});
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+ 
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final MainModel mainModel = ref.watch(mainProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '${mainModel.counter}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: mainModel.incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
