2e9ee334a77b9b16e2374bf66843acc2756cbeaa
Toma Morishita <25winterhorse@gmail.com>
2024-11-15 00:01:25 +0900

    firebaseと接続、非同期処理

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
+// flutter
 import 'package:flutter/material.dart';
-import 'package:flutter_application/main_model.dart';
+// packages
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+// models
+import 'main_model.dart';
+// options
+import 'firebase_options.dart';
  
-void main() {
-  const String title = 'Flutter Demo';
-  runApp(const ProviderScope(child: MyApp(title: title,)));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ProviderScope(child: MyApp()));
 }
  
 class MyApp extends StatelessWidget {
-  const MyApp({super.key,required this.title});
-  final String title;
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: title,
+      title: "Flutter Demo",
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -27,7 +35,7 @@ class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context,WidgetRef ref) {
     final MainModel mainModel = ref.watch(mainProvider);
     return Scaffold(
       appBar: AppBar(
